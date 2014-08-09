cb3cbc9e146ae577a9860155e1334d588e4a2708
aszkid <polgomezriquelme@gmail.com>
2014-08-09 17:03:06 +0200

    - Request OGL 3.2 core forward-compat context. - Print some debug info.

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,22 @@
 
 #include <stdio.h>
 
+#define GL_MAJOR 3
+#define GL_MINOR 2
+
 int main(int argc, char **argv)
 {
 	GLFWwindow *win;
 	
 	if(!glfwInit())
 		return -1;
+	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_MAJOR);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_MINOR);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 	
 	win = glfwCreateWindow(640, 480, "Hello Window", NULL, NULL);
 	if(!win) {
@@ -22,10 +32,12 @@ int main(int argc, char **argv)
 	GLenum err = glewInit();
 	if(GLEW_OK != err) {
 		glfwTerminate();
-		printf("Could not initialize GLEW!\n");
+		printf("GLEW error: %s.\n", glewGetErrorString(err));
 		return -1;
 	}
 	
+	printf("OpenGL Version: %s.\n", glGetString(GL_VERSION));
+	printf("GLEW Version:   %s.\n", glewGetString(GLEW_VERSION));
 	
 	while(!glfwWindowShouldClose(win)) {
 		// render
