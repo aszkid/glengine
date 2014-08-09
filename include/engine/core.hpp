f1fa68cc883b0c688d8be71613c1075f6bded8ca
aszkid <polgomezriquelme@gmail.com>
2014-08-09 13:56:53 +0200

    - Initial commit

---
+++ include/engine/core.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#define LOG(to, str) to << str
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <engine/core.hpp>
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	LOG(std::cout, "Hello world: " << 42 << std::endl);
+
+	return 0;
+}
