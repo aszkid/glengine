62b7e529241cf9204d98ed401ff3b9d9a70e3aeb
aszkid <polgomezriquelme@gmail.com>
2014-08-09 18:12:38 +0200

    - #define window size - Escape key closes window - Get normalized mouse position

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,27 @@
 #define GL_MAJOR 3
 #define GL_MINOR 2
 
+#define WIN_WIDTH  640
+#define WIN_HEIGHT 480
+
+void key_callback(GLFWwindow *win, int key, int scancode, int action, int mods)
+{
+	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(win, GL_TRUE);
+}
+
+glm::vec2 mouse_pos(GLFWwindow *win)
+{
+	glm::vec2 ret;
+	double x, y;
+	glfwGetCursorPos(win, &x, &y);
+
+	ret.x = x / float(WIN_WIDTH);
+	ret.y = y / float(WIN_HEIGHT);
+	
+	return ret;
+}
+
 int main(int argc, char **argv)
 {
 	GLFWwindow *win;
@@ -21,7 +42,7 @@ int main(int argc, char **argv)
 	
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 	
-	win = glfwCreateWindow(640, 480, "Hello Window", NULL, NULL);
+	win = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Hello Window", NULL, NULL);
 	if(!win) {
 		glfwTerminate();
 		printf("Could not create window!\n");
@@ -40,14 +61,32 @@ int main(int argc, char **argv)
 	printf("OpenGL Version: %s.\n", glGetString(GL_VERSION));
 	printf("GLEW Version:   %s.\n", glewGetString(GLEW_VERSION));
 	
+	glfwSetKeyCallback(win, key_callback);
+	
+	double time = glfwGetTime(), ntime;
+	glm::vec2 mpos;
+	
 	while(!glfwWindowShouldClose(win)) {
-		// render
+		// render stuff
+		// ------
+		// setup3d() render3d()
+		// setup2d() render2d()
+		// ------
 		
-		// swap
+		// swap buffers
 		glfwSwapBuffers(win);
+		// ------
 		
-		// poll
+		// poll events
 		glfwPollEvents();
+		
+		ntime = glfwGetTime();
+		if((ntime - time) > 0.5) {
+			time = ntime;
+			mpos = mouse_pos(win);
+			printf("X: %f | Y: %f\n", mpos.x, mpos.y);
+		}
+		// ------
 	}
 	
 	glfwTerminate();
